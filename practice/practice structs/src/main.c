cf8041bba5eba8622eee92f4aff31ed19ac3beb8
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-05-07 11:01:42 +0200

    struct practice

--- practice/practice structs/src/main.c
+++ practice/practice structs/src/main.c
@@ -3,13 +3,34 @@
 #include <string.h>
 
 typedef struct {
-    char make[15];
-    char model[15];
-    int year;
+  char make[15];
+  char model[15];
+  int year;
 } CAR;
+
+typedef CAR GARAGE[10];
+
+typedef struct {
+  char name[20];
+  int numberOfCars;
+  GARAGE garage;
+} DEALER;
 
 void printCar(CAR car){
   printf("make: %s, model: %s, year: %d \n", car.make, car.model, car.year);
+}
+
+void printGarage (GARAGE garage, int numberOfCars){
+  for (int i = 0; i < numberOfCars; i++ ){
+    printf("make: %s, model: %s, year: %d \n", garage[i].make, garage[i].model, garage[i].year);
+  }
+}
+
+printDealer(DEALER dealer) {
+  printf("Dealer %s has the following %d cars: \n", dealer.name, dealer.numberOfCars);
+  for (int i = 0; i < dealer.numberOfCars; i++ ){
+    printf("make: %s, model: %s, year: %d \n", dealer.garage[i].make, dealer.garage[i].model, dealer.garage[i].year);
+  }
 }
 
 int main() {
@@ -27,9 +48,14 @@ int main() {
 
   CAR car3 = car1;
 
-  printCar(car1);
-  printCar(car2);
+  CAR car4 = {"Honda", "Civic", 1999};
 
-  printCar(car3);
-  printCar(car1);
+  GARAGE garage1 = {car1, car2, car3, car4};
+
+  DEALER dealer1 = {"Szymon", 4, {car1, car2, car3, car4}}; 
+
+  printGarage(garage1, 4);
+  printf("\n\n");
+
+  printDealer(dealer1);
 }
