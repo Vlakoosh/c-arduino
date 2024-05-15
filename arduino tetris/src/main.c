c72c818b165924c20ce762342f9b05c915f86c06
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-05-15 10:48:11 +0200

    made most of the code for led matrix display library (with shift registers)

--- arduino tetris/src/main.c
+++ arduino tetris/src/main.c
@@ -5,47 +5,139 @@
 #include <string.h>
 #include <usart.h>
 
-#define DATA_PIN_1 PD0
-#define DATA_PIN_2 PD3
-#define CLOCK_INTERNAL_1 PD1
-#define CLOCK_INTERNAL_2 PD4
-#define CLOCK_1 PD2
-#define CLOCK_2 PD5
+#define DATA_PIN_1 PD2
+#define DATA_PIN_2 PD5
+#define PUSH_CLOCK_1 PD3
+#define PUSH_CLOCK_2 PD6
+#define UPDATE_CLOCK_1 PD4
+#define UPDATE_CLOCK_2 PD7
 
-// void cleanDisplay(){
+#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
-// }
-void lightUpWholeDisplay(){
-  PORTD &= ~(1 << CLOCK_2);
-  printf("turning off the master clock\n");
-  _delay_ms(50);
-  PORTD &= ~(1 << CLOCK_INTERNAL_2);
-  _delay_ms(50);
+void lightUpWholeMatrix(){
+  //light down the clocks
+  PORTD &= ~(1 << UPDATE_CLOCK_1);
+  PORTD &= ~(1 << UPDATE_CLOCK_2);
+
+  PORTD &= ~(1 << PUSH_CLOCK_1);
+  PORTD &= ~(1 << PUSH_CLOCK_2);
+
+  PORTD &= ~(1 << DATA_PIN_1);
+  PORTD |= (1 << DATA_PIN_2);
   
+  for (int i = 0; i < 8; i++)
+  {
+    //push LOW bits for columns, and HIGH bits for rows
+    PORTD |= (1 << PUSH_CLOCK_1);
+    PORTD |= (1 << PUSH_CLOCK_2);
 
-  PORTD |= (1 << DATA_PIN_2);
-  _delay_ms(50);
-  PORTD |= (1 << DATA_PIN_2);
-  _delay_ms(50);
+    PORTD &= ~(1 << PUSH_CLOCK_1);
+    PORTD &= ~(1 << PUSH_CLOCK_2);
+  }
+  //update the output
+  PORTD |= (1 << UPDATE_CLOCK_1);
+  PORTD |= (1 << UPDATE_CLOCK_2);
+}
+
+void lightDownWholeMatrix(){
+  //light down the clocks
+  PORTD &= ~(1 << UPDATE_CLOCK_1);
+  PORTD &= ~(1 << UPDATE_CLOCK_2);
+
+  PORTD &= ~(1 << PUSH_CLOCK_1);
+  PORTD &= ~(1 << PUSH_CLOCK_2);
+
+  PORTD |= (1 << DATA_PIN_1);
+  PORTD &= ~(1 << DATA_PIN_2);
+  
+  for (int i = 0; i < 8; i++)
+  {
+    //push LOW bits for columns, and HIGH bits for rows
+    PORTD |= (1 << PUSH_CLOCK_1);
+    PORTD |= (1 << PUSH_CLOCK_2);
+
+    PORTD &= ~(1 << PUSH_CLOCK_1);
+    PORTD &= ~(1 << PUSH_CLOCK_2);
+  }
+  //update the output
+  PORTD |= (1 << UPDATE_CLOCK_1);
+  PORTD |= (1 << UPDATE_CLOCK_2);
+}
+
+void setRow(int row){
+  if (row < 1 || row > 8) return;
+  row = row-1;
+  
+  //light down the clocks
+  PORTD &= ~_BV(UPDATE_CLOCK_2);
+
+  PORTD &= ~_BV(PUSH_CLOCK_2);
+
+  PORTD |= _BV(DATA_PIN_2);
 
   for (int i = 0; i < 8; i++)
   {
-    printf("internal clock pulse #%d\n", i);
-    PORTD |= (1 << CLOCK_INTERNAL_2);
-    _delay_ms(200);
-    PORTD &= ~(1 << CLOCK_INTERNAL_2);
-    _delay_ms(200);
+    if (row == i)
+    {
+      PORTD |= _BV(DATA_PIN_2);
+    }
+    else PORTD &= ~_BV(DATA_PIN_2);
+    PORTD |= _BV(PUSH_CLOCK_2);
+    PORTD &= ~_BV(PUSH_CLOCK_2);
   }
-  _delay_ms(50);
-  PORTD |= (1 << CLOCK_2);
-  
-  
-  printf("turning back on the master clock\n");
-
+  PORTD |= _BV(UPDATE_CLOCK_2);
   
 }
 
+void setColumn(int col){
+  if (col < 1 || col > 8) return;
+  col = col-1;
+  
+  //light down the clocks
+  PORTD &= ~_BV(UPDATE_CLOCK_1);
 
+  PORTD &= ~_BV(PUSH_CLOCK_1);
+
+  PORTD |= _BV(DATA_PIN_1);
+
+  for (int i = 0; i < 8; i++)
+  {
+    if (col == i)
+    {
+      PORTD &= ~_BV(DATA_PIN_1);
+    }
+    else PORTD |= _BV(DATA_PIN_1);
+    PORTD |= _BV(PUSH_CLOCK_1);
+    PORTD &= ~_BV(PUSH_CLOCK_1);
+  }
+  PORTD |= _BV(UPDATE_CLOCK_1);
+}
+
+void setMultipleColumns(uint8_t cols){
+  //light down the clocks
+  PORTD &= ~_BV(UPDATE_CLOCK_1);
+
+  PORTD &= ~_BV(PUSH_CLOCK_1);
+
+  PORTD |= _BV(DATA_PIN_1);
+
+  for (int i = 0; i < 8; i++)
+  {
+    if (cols & (1<<i))
+    {
+      PORTD &= ~_BV(DATA_PIN_1);
+    }
+    else PORTD |= _BV(DATA_PIN_1);
+    PORTD |= _BV(PUSH_CLOCK_1);
+    PORTD &= ~_BV(PUSH_CLOCK_1);
+  }
+  PORTD |= _BV(UPDATE_CLOCK_1);
+  _delay_ms(1);
+}
+
+void displayRow(int row, uint8_t columns){
+  setRow(row);
+}
 
 // void displayMatrix(int matrix[][]){
 
@@ -65,15 +157,40 @@ int main(){
 
   DDRD |= (1 << DATA_PIN_1);
   DDRD |= (1 << DATA_PIN_2);
-  DDRD |= (1 << CLOCK_1);
-  DDRD |= (1 << CLOCK_2);
-  DDRD |= (1 << CLOCK_INTERNAL_1);
-  DDRD |= (1 << CLOCK_INTERNAL_2);
+  DDRD |= (1 << UPDATE_CLOCK_1);
+  DDRD |= (1 << UPDATE_CLOCK_2);
+  DDRD |= (1 << PUSH_CLOCK_1);
+  DDRD |= (1 << PUSH_CLOCK_2);
 
-  PORTD &= ~(1 << CLOCK_INTERNAL_1);
-  PORTD &= ~(1 << CLOCK_INTERNAL_2);
+  PORTD &= ~(1 << PUSH_CLOCK_1);
+  PORTD &= ~(1 << PUSH_CLOCK_2);
 
   initUSART();
+  while (1)
+  {
+    // lightDownWholeMatrix();
+    // _delay_ms(500);
+    // lightUpWholeMatrix();
+    // _delay_ms(500);
+    setRow(1);
+    setMultipleColumns((uint8_t) 0b00011000);
+    setRow(2);
+    setMultipleColumns((uint8_t) 0b00111100);
+    setRow(3);
+    setMultipleColumns((uint8_t) 0b01111110);
+    setRow(4);
+    setMultipleColumns((uint8_t) 0b11011011);
+    setRow(5);
+    setMultipleColumns((uint8_t) 0b11111111);
+    setRow(6);
+    setMultipleColumns((uint8_t) 0b00111100);
+    setRow(7);
+    setMultipleColumns((uint8_t) 0b01111110);
+    setRow(8);
+    setMultipleColumns((uint8_t) 0b10100101);
 
-  lightUpWholeDisplay();
+    
+    
+    
+  }
 }
